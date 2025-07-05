edec08d98f741afc77c15db4ed2fe8cb0b775685
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-07-05 12:09:40 +0530

    logged in user doesn't navigate to the login

--- actions/auth.ts
+++ actions/auth.ts
@@ -7,6 +7,20 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 
+export async function getUserSessions() {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) {
+        return null;
+    }
+
+    return {
+        status: "success",
+        user: data?.user
+    };
+}
+
 export async function signUp(formData: FormData){
     const supabase = await createClient();
 
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { getUserSessions } from "@/actions/auth";
+import { redirect } from "next/navigation";
+
 export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const response = await getUserSessions();
+  if(response?.user) {
+    redirect("/");
+  }
   return <>{children}</>;
 }
